2a75094b84968165cb4c275f983ffaaa3e992eb4
lijian575 <lijian53@jd.com>
2020-10-15 21:02:18 +0800

    first

---
+++ seckill-soa/src/main/java/com/jd/seckill/soa/stock/SkDeductStock.java
@@ -0,0 +1,10 @@
+package com.jd.seckill.soa.stock;
+
+/**
+ * describe:
+ *
+ * @author lijian575
+ * @date 2020/10/15
+ */
+public interface SkDeductStock {
+}
---
+++ seckill-soa/src/main/java/com/jd/seckill/soa/stock/impl/SkFillStockImpl.java
@@ -0,0 +1,12 @@
+package com.jd.seckill.soa.stock.impl;
+
+import com.jd.seckill.soa.stock.SkFillStock;
+
+/**
+ * describe:
+ *
+ * @author lijian575
+ * @date 2020/10/15
+ */
+public class SkFillStockImpl implements SkFillStock {
+}
